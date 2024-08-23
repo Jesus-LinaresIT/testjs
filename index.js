e12afa2b282920d6ff282e4a61e250bdf44a5520
itboxmarketing <it@urbancity.com.sv>
2024-08-23 09:08:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts'; // URL de ejemplo
+
+const getData = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    console.log('Datos recibidos:', response.data);
+  } catch (error) {
+    console.error('Error al realizar la consulta:', error);
+  }
+}
+
+getData();
+
+async function postData() {
+   try {
+     const response = await axios.post(API_URL, {
+       title: 'foo',
+       body: 'bar',
+       userId: 1
+     });
+     console.log('Respuesta del POST:', response.data);
+   } catch (error) {
+     console.error('Error en el POST:', error);
+   }
+ }
+ 
+ postData();
